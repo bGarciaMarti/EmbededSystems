c1d4c836d75f2f4efdc09e939023f9f4c56ae1db
bGarciaMarti <98002963+bGarciaMarti@users.noreply.github.com>
2024-09-14 09:13:03 -0600

    success LCD "Howdy world"

---
+++ lab1/Objects/heartbeat.d
@@ -0,0 +1,12 @@
+.\objects\heartbeat.o: heartbeat.c
+.\objects\heartbeat.o: user_func.h
+.\objects\heartbeat.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\heartbeat.o: ..\Include\Driver\DrvTimer.h
+.\objects\heartbeat.o: ..\NUC1xx\NUC1xx.h
+.\objects\heartbeat.o: ..\CoreSupport\core_cm0.h
+.\objects\heartbeat.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\heartbeat.o: ..\NUC1xx\system_NUC1xx.h
+.\objects\heartbeat.o: ..\Include\System\SysInfra.h
+.\objects\heartbeat.o: ..\Include\System\ModuleID.h
+.\objects\heartbeat.o: ..\Include\Driver\DrvSYS.h
+.\objects\heartbeat.o: ..\Include\Driver\DrvGPIO.h
